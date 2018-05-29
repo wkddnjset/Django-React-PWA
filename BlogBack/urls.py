2a2df345efd5322bcec927f30aa37d019a2e3dba
Jangwon <wkddnjset@gmail.com>
2018-05-29 11:18:36 +0900

    Clone ReactJS

--- BlogBack/urls.py
+++ BlogBack/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('PostAPI.api.urls', namespace='api'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ blogfront/src/reducers/reducer_lists.js
@@ -0,0 +1,15 @@
+import { FETCH_POST } from '../actions/getPostList';
+
+const initialState = {
+    postList: [],
+};
+
+
+export default function (state = initialState, action) {
+	switch(action.type) {
+		case FETCH_POST:
+			return { ...state, postList: action.payload.data };
+		default:
+			return state;
+	}
+}
